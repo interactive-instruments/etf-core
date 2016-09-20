41bf5a63d875273ed821c42c8fe7d8b2f548ca44
Jon Herrmann <herrmann@interactive-instruments.de>
2016-09-20 13:31:05 +0200

    Split TestRunLogger iface into Logger + Reader

--- src/main/java/de/interactive_instruments/etf/testdriver/TestRunLogReader.java
+++ src/main/java/de/interactive_instruments/etf/testdriver/TestRunLogReader.java
@@ -1,11 +1,11 @@
-/**
- * Copyright 2010-2016 interactive instruments GmbH
+/*
+ * Copyright ${year} interactive instruments GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,19 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package de.interactive_instruments.etf.testdriver;
 
-import org.slf4j.Logger;
-
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
 /**
- * Logger used by TestDrivers
+ * Log file read interface
  *
  * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
  */
-public interface TestRunLogger extends TestRunLogReader, Logger {
+public interface TestRunLogReader {
+	/**
+	 * Returns the logger that is used to log information about
+	 * the progress of the task
+	 *
+	 * @return
+	 */
+	File getLogFile();
+
+	/**
+	 * Return messages
+	 *
+	 * @param knownPosition
+	 * @return
+	 */
+	List<String> getLogMessages(final long knownPosition);
+
+	/**
+	 * Stream messages to an OutputStream
+	 *
+	 * @param knownPosition
+	 * @param outputStream
+	 */
+	void streamLogMessagesTo(final long knownPosition, final OutputStream outputStream);
 }
