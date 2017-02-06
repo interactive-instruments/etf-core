ba04afa1ac1940f6b7e932345556879e90dc4586
Jon Herrmann <herrmann@interactive-instruments.de>
2017-02-06 19:29:54 +0100

    ResultCollector

--- src/main/java/de/interactive_instruments/etf/testdriver/BasicTestResultCollector.java
+++ src/main/java/de/interactive_instruments/etf/testdriver/BasicTestResultCollector.java
@@ -1,7 +1,120 @@
+/**
+ * Copyright 2010-2016 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.interactive_instruments.etf.testdriver;
 
+import de.interactive_instruments.Releasable;
+import de.interactive_instruments.etf.dal.dto.result.TestResultStatus;
+
 /**
+ * Basic Test Result Collector interface
+ *
  * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
  */
-public interface BasicTestResultCollector {
+public interface BasicTestResultCollector extends Releasable {
+
+	/**
+	 * Called when a Test Task is run
+	 *
+	 * @param testTaskId Test Task EID
+	 * @param startTimestamp start timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if the eid is invalid or the test can't be started in the current context
+	 * @throws IllegalStateException if test already has been started or ended
+	 */
+	String startTestTask(final String testTaskId, long startTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Called when a Test Module is run
+	 *
+	 * @param testModuleId Test Module EID
+	 * @param startTimestamp start timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if the eid is invalid or the test can't be started in the current context
+	 * @throws IllegalStateException if test already has been started or ended
+	 */
+	String startTestModule(final String testModuleId, long startTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Called when a Test Case is run
+	 *
+	 * @param testCaseId Test Case EID
+	 * @param startTimestamp start timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if the eid is invalid or the test can't be started in the current context
+	 * @throws IllegalStateException if test already has been started or ended
+	 */
+	String startTestCase(final String testCaseId, long startTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Called when a Test Step is run
+	 *
+	 * @param testStepId Test Step EID
+	 * @param startTimestamp start timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if the eid is invalid or the test can't be started in the current context
+	 * @throws IllegalStateException if test already has been started or ended
+	 */
+	String startTestStep(final String testStepId, long startTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Called when a Test Assertion is run
+	 *
+	 * @param testAssertionId Test Assertion EID
+	 * @param startTimestamp start timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if the eid is invalid or the test can't be started in the current context
+	 * @throws IllegalStateException if test already has been started or ended
+	 */
+	String startTestAssertion(final String testAssertionId, long startTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Called just after a test item has been run
+	 *
+	 * @param testModelItemId Test Model Item EID
+	 * @param status {@link TestResultStatus} as integer
+	 * @param stopTimestamp stop timestamp
+	 *
+	 * @return eid of the recorded test result item
+	 *
+	 * @throws IllegalArgumentException if test already has been ended
+	 * @throws IllegalStateException if test already has been ended or hasn't been started yet
+	 */
+	String end(final String testModelItemId, final int status, long stopTimestamp) throws IllegalArgumentException, IllegalStateException;
+
+	/**
+	 * Returns the currently recorded model type
+	 *
+	 * 1 for Test Task
+	 * 2 for Test Module
+	 * 3 for Test Case
+	 * 4 for Test Step
+	 * 5 for Test Assertion
+	 * -1 for undefined
+	 *
+	 * @return model type as integer (1-5) or -1 for undefined
+	 */
+	int currentModelType();
 }
