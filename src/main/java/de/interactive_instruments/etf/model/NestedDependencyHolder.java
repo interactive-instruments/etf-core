7d61422356506475d4f65496fc6b34dba19a23f4
Jon Herrmann <herrmann@interactive-instruments.de>
2017-02-06 19:27:24 +0100

    Dependency types

--- src/main/java/de/interactive_instruments/etf/model/NestedDependencyHolder.java
+++ src/main/java/de/interactive_instruments/etf/model/NestedDependencyHolder.java
@@ -1,11 +1,11 @@
-/*
- * Copyright ${year} interactive instruments GmbH
+/**
+ * Copyright 2010-2016 interactive instruments GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,14 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.interactive_instruments.etf.model;
-
-import java.util.Collection;
 
 /**
  * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
  */
-public interface DependencyHolder<T extends DependencyHolder> {
-	Collection<T> getDependencies();
-}
+public interface NestedDependencyHolder<T extends NestedDependencyHolder> extends FlatDependencyHolder<T> {}
