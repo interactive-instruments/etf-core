d6a361110e3f24c7afcd56754334a677b94ac59f
Jon Herrmann <herrmann@interactive-instruments.de>
2019-01-14 17:00:00 +0100

    forkreadme
    Signed-off-by: Jon Herrmann <herrmann@interactive-instruments.de>

--- src/main/java/de/interactive_instruments/etf/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <p>Core package for ETF</p>
- * <img src="package.svg" alt="Package UML">
- */
-package de.interactive_instruments.etf;
--- src/main/java/de/interactive_instruments/etf/testdriver/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <p>Classes for controlling Test Runs</p>
- * <img src="package.svg" alt="Package UML">
- */
-package de.interactive_instruments.etf.testdriver;
